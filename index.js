ca7795ed745d90983f4dd3c39abfd5022ab14774
JareCoder <jesse.m.lappalainen@gmail.com>
2024-10-20 17:17:13 +0300

    Completed up to 3.18 task

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const morgan = require('morgan')
 const Person = require('./models/person')
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :body'))
-app.use(express.static('dist'))
 
 let persons = [
     {
@@ -38,33 +38,37 @@ app.get('/', (req, res) => {
     res.send('<h1>I am not broken :)</h1>')
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
     Person.find({}).then(result => {
         res.json(result)
-    })
+    }).catch(e => next(e))
 })
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
     const date = new Date()
     res.send(`
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${date}</p>
-    `)
+    `).catch(e => next(e))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id).then(person => {
-        res.json(person)
-    })
+        if(person){
+            res.json(person)
+        }else{
+            res.status(404).end()
+        }
+    }).catch(e => next(e))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndDelete(req.params.id).then(result => {
+        res.status(204).end()
+    }).catch(e => next(e))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     if(!body.name){
@@ -77,22 +81,51 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    if(persons.find(person => person.name === body.name)){
+    /*const existingPerson = await Person.findOne({name: body.name})
+
+    if(existingPerson){
         return res.status(400).json({
             error: 'Name must be unique'
         })
-    }
+    }*/
 
-    const person = {
+    const person = new Person({
         id: Math.floor(Math.random() * 1000),
         name: body.name,
         number: body.number
-    }
+    })
     
-    persons = persons.concat(person)
-
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    }).catch(e => next(e))
 })
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: body.id,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, {new: true}).then(updatedPerson => {
+        res.json(updatedPerson)
+    }).catch(e => next(e))
+})
+
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if(error.name === 'CastError'){
+        return res.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
-  }
-  
-  const password = process.argv[2]
-
 const url = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(result => {
@@ -16,14 +9,14 @@ mongoose.connect(url).then(result => {
 })
 
 const personSchema = new mongoose.Schema({
-    id: Number,
     name: String,
     number: String,
+    id: String,
   })
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-        returnedObject._id = returnedObject._id.toString()
+        returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
     }
